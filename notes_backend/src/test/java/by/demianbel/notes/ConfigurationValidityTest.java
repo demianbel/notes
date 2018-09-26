604905c48b4b886fea532867fa35c7cdadd6f6e2
d.belsky <demianbel@gmail.com>
2018-09-26 22:28:08 +0300

    add configuration validity test and h2

---
+++ notes_backend/src/test/java/by/demianbel/notes/ConfigurationValidityTest.java
@@ -0,0 +1,19 @@
+package by.demianbel.notes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("integration")
+public class ConfigurationValidityTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+
+}
